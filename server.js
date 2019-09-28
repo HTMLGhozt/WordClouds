e505bf8cd2f07b8cb368846eab00bccd79278f63
Thomas Dillard <thomas.dillard.studios@gmail.com>
2019-09-27 22:29:08 -0400

    Monoserver setup
    - Setup server on :8000
    - Setup /api/site route
    - Setup query params on /api/site

--- server.js
+++ server.js
@@ -1,25 +1,108 @@
+const http = require('http');
 const https = require('https');
+const fs = require('fs');
+const url = require('url');
+const path = require('path');
+const querystring = require('querystring');
 const stream = require('stream');
 
-const request = https.get('https://www.google.com/', res => {
-  // console.log('statusCode:', res.statusCode);
-  // console.log('headers:', res.headers);
-  res.setEncoding('utf8')
-  let data = '';
-  res.on('data', chunk => {
-    data += chunk;
-  });
+const mimeType = {
+  '.ico': 'image/x-icon',
+  '.html': 'text/html',
+  '.js': 'text/javascript',
+  '.json': 'application/json',
+  '.css': 'text/css',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
+  '.wav': 'audio/wav',
+  '.mp3': 'audio/mpeg',
+  '.svg': 'image/svg+xml',
+  '.pdf': 'application/pdf',
+  '.doc': 'application/msword',
+  '.eot': 'appliaction/vnd.ms-fontobject',
+  '.ttf': 'aplication/font-sfnt'
+};
 
-  res.on('end', () => {
-    const bodyStart = data.indexOf('<body');
-    const bodyEnd = data.lastIndexOf('</body>');
-    const body = data.substring(bodyStart, bodyEnd);
-    const moreParsedBody = body.replace(/\<script[\s\S]*?\/script>/g, '');
-    console.log(moreParsedBody);
-    // data.replace(new RegExp('(<body[\s\S]*/body>)'), (...args) => console.log(args));
-  });
+const server = http.createServer((request, response) => {
+  console.log(`${request.method} ${request.url}`);
+
+  const parsedUrl = url.parse(request.url);
+
+  const sanitizedPath = path
+    .normalize(parsedUrl.pathname)
+    .replace(/^(\.\.[\/\\])+/, '');
+
+  const isApi = /^\/api\//.test(sanitizedPath);
+
+  if (isApi) {
+    const subpath = /^\/api\/([^\/]+)\//.exec(sanitizedPath)[1];
+    
+    switch (subpath) {
+      case 'site':
+        const { site } = querystring.parse(parsedUrl.query);
+        if (site) return getBodyFromUrl(response, site);
+        break;
+      default:
+    }
+  }
+
+  let pathname = path.join(__dirname, sanitizedPath);
+  
+  if (!fs.existsSync(pathname)) {
+    response.statusCode = 404;
+    response.end(`${sanitizedPath} doesn't exist`);
+    return;
+  }
+
+  if (fs.statSync(pathname).isDirectory()) {
+    pathname += '/index.html';
+    pathname = pathname.replace(/\/\//g, '/');
+  }
+
+  fs.readFile(pathname, (error, data) => {
+    if (error) {
+      response.statusCode = 500;
+      response.end(`Error getting ${sanitizedPath}`);
+      return;
+    }
+
+    const { ext } = path.parse(pathname);
+    response.setHeader('Content-type', mimeType[ext] || 'text/plain');
+    response.end(data);
+  })
 });
 
-request.on('error', error => {
-  console.error(error);
+const port = process.env.PORT || 8000;
+
+server.listen(port, () => {
+  console.info(`Server listening on port: ${port}.`);
 });
+
+const getBodyFromUrl = (res, site) => {
+  console.info(`Fetching site: ${site}`);
+  try {
+    https.get(site, response => {
+      const { statusCode } = response;
+      console.log(`Status code: ${statusCode}`);
+      response.setEncoding('utf8');
+      let data = '';
+      response.on('data', chunk => {
+        data += chunk;
+      });
+
+      response.on('end', () => {
+        const body = data
+          .substring(data.indexOf('<body'), data.lastIndexOf('</body>'))
+          .replace(/\<script[\s\S]*?\/script>/g, '');
+
+        res.setHeader('Content-type', 'text/plain');
+        res.end(body);
+      });
+    });
+  } catch (error) {
+    console.error(site, error.message);
+    res.statusCode = 500;
+    res.end(`Error getting ${site}`);
+  }
+};
+
